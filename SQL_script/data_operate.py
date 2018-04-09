e35c404eb67863df77221ebd6ee2f81c2d078fa8
x@hexagra.vip <x@hexagram.vip>
2018-04-09 11:42:58 +0800

    修改SQL_script/data_operate.py文件：减少打印信息量，省略数据库查重步骤，提高MySQL插入速度； 添加.gitignore文件。

--- SQL_script/data_operate.py
+++ SQL_script/data_operate.py
@@ -2,6 +2,18 @@ import time
 
 from officer_process import Con_MySQL, Con_PostgreSQL, Con_Neo4j
 
+def time_now(type_int=0):
+    """
+    函数功能：匹配时间段重叠情况。
+    时间格式统一为‘1995-4-16’。
+    :param type_int: 时间类型，0：'1995-4-16'，1：'Sun Apr 16 6:6:6 1995'
+    :return: 请求的格式化时间
+    """
+    if type_int == 0:
+        return str(time.localtime(time.time()).tm_year) + '-' + str(time.localtime(time.time()).tm_mon) + '-' \
+               + str(time.localtime(time.time()).tm_mday)
+    if type_int == 1:
+        return time.asctime(time.localtime(time.time()))
 
 def data_migration(rel_type=None, relation=None, relationstr=None):
     """
@@ -11,7 +23,7 @@ def data_migration(rel_type=None, relation=None, relationstr=None):
     :param relationstr: 关系型数据库中的关系名称
     :return: True
     """
-    select_mysql = "SELECT * FROM demo.people_rel WHERE people_id='{0}' AND link_id='{1}' AND relation={2};"
+    # select_mysql = "SELECT * FROM demo.people_rel WHERE people_id='{0}' AND link_id='{1}' AND relation={2};"
     insert_mysql = "INSERT INTO demo.people_rel(people_id,link_id,people_name,link_name,intimate,relation,relationstr) VALUES ('{0}','{1}','{2}','{3}','{4}',{5},'{6}');"
     cur_mysql = mysql.cursor()
     # cur_postgre = postgre.cursor()
@@ -21,20 +33,23 @@ def data_migration(rel_type=None, relation=None, relationstr=None):
     i = count = 0  # 计数器
     for result in results:
         i += 1
-        print(i, result['a']['name'], result['b']['name'])
+        # print(i, result['a']['name'], result['b']['name'])
         intimate = int(4*(float(result['r']['cost'])-min_value)/(max_value-min_value))+1  # 亲密度标准化
-        cur_mysql.execute(select_mysql.format(result['a']['id'], result['b']['id'], relation))
-        if not cur_mysql.fetchone():  # 如果记录不存在，则插入
-            finish_insert_sql = insert_mysql.format(result['a']['id'], result['b']['id'], result['a']['name'],
-                                                    result['b']['name'], intimate, relation, relationstr)
-            try:
-                cur_mysql.execute(finish_insert_sql)
-                mysql.commit()
-                count += 1
-                print(count, 'MySQL Insert Successful:', result['a']['id'], result['b']['id'], result['a']['name'], result['b']['name'], intimate, relation, relationstr)
-            except:
-                mysql.rollback()  # 插入失败，执行回滚操作
-                print('\t\tMySQL Insert Error:', result['a']['id'], result['b']['id'], result['a']['name'], result['b']['name'], intimate, relation, relationstr)
+        # cur_mysql.execute(select_mysql.format(result['a']['id'], result['b']['id'], relation))
+        # if not cur_mysql.fetchone():  # 如果记录不存在，则插入
+        finish_insert_sql = insert_mysql.format(result['a']['id'], result['b']['id'], result['a']['name'],
+                                                result['b']['name'], intimate, relation, relationstr)
+        try:
+            cur_mysql.execute(finish_insert_sql)
+            mysql.commit()
+            count += 1
+            if count % 1000 == 0:
+                print(time_now(), 'All:', i, 'Insert:', count)
+            # print(time_now(), count, 'MySQL Insert Successful:', result['a']['id'], result['b']['id'], result['a']['name'], result['b']['name'], intimate, relation, relationstr)
+        except:
+            mysql.rollback()  # 插入失败，执行回滚操作
+            print('\t\ttime_now(), MySQL Insert Error:', result['a']['id'], result['b']['id'], result['a']['name'], result['b']['name'], intimate, relation, relationstr)
+    print(time_now(), 'All:', i, 'Insert:', count)
     cur_mysql.close()
     # cur_postgre.close()
     return True
@@ -47,7 +62,7 @@ if __name__ == '__main__':
     mysql = Con_MySQL(**option_mysql)
     postgre = Con_PostgreSQL(**option_postgre)
     graph = Con_Neo4j(**option_neo4j)
-    # data_migration('workmate_with', 4, '同事')  # 导入同事关系
+    data_migration('workmate_with', 4, '同事')  # 导入同事关系
     data_migration('schoolfellow_with', 7, '同学')  # 导入同学关系
     mysql.close()
     postgre.close()
